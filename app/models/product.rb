52091778fd125d965a8018409cb57baecdda4c5b
vegeta <vegeta@rework.com>
2015-03-14 00:01:53 +0800

    Add product index and show html

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,4 +15,33 @@ class Product  < ActiveRecord::Base
   def applicable_types_string=(value)
     self.applicable_types = value.split(",").map { |t| t.strip }
   end
+
+  def trans_title
+    if send(locale_prefix('title')).present?
+      send(locale_prefix('title'))
+    else
+      zh_TW_title
+    end
+  end
+
+  def trans_content
+    if send(locale_prefix('content')).present?
+      send(locale_prefix('content'))
+    else
+      zh_TW_content
+    end
+  end
+
+  def trans_description
+    if send(locale_prefix('description')).present?
+      send(locale_prefix('description'))
+    else
+      zh_TW_description
+    end
+  end
+
+  private
+  def locale_prefix(column_name)
+    I18n.locale.to_s.downcase.gsub('-', '_') + '_' + column_name
+  end
 end
