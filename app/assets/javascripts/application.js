ea699625a2536678cb099aba0aff76a65597f34b
vegeta <vegeta@rework.com>
2015-03-18 19:21:53 +0800

    Fix boostrap require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require ckeditor-jquery
 //= require jquery-fileupload/basic
 //= require_tree .
