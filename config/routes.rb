88ac91ffac07b1d52b088139266d3b40979fe35a
vegeta <vegeta@rework.com>
2015-03-18 16:27:28 +0800

    Add state machine gem

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'index#index'
 
+  get 'contact' => 'contact#index', as: :contact
+
   resources :products, only: [:index, :show]
 
   # Example of regular route:
